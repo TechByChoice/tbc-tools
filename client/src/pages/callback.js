3ad0066f220ed755126aee6e501a72071a17a971
valerie <valeriesharp93@gmail.com>
2019-11-02 21:15:49 -0800

    Auth0 flow added to application
    - created protected urls
    - added login flow

---
+++ client/src/pages/callback.js
@@ -0,0 +1,17 @@
+import React                    from "react"
+import { handleAuthentication } from "../utils/auth"
+import Layout                   from "../components/layout"
+
+const Callback = () => {
+  handleAuthentication()
+
+  return (
+    <>
+      <Layout>
+        <p>Loading...</p>
+      </Layout>
+    </>
+  )
+}
+
+export default Callback
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,22 +4,17 @@ import { Link } from "gatsby"
 import Layout       from "../components/layout"
 import Image        from "../components/image"
 import SEO          from "../components/seo"
-// import Directory    from "../components/Directory"
-// import MeetupGroups from "../components/MeetupGroups"
-import MeetupEvents from "./MeetupEvents"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Tech By Choice Event Management</h1>
-    <p>Make event planning simple by creating &amp; tracking events here</p>
-    <p>Useing Eventbrite, Meetup, or something new? We got you covered.</p>
+    <h1>Tech By Choice </h1>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/MeetupEvents/">View Meetups</Link>
-    <Link to="/Eventbrite/">View Eventbrite</Link>
+    <Link to="/Login">Login</Link>
+      <br/>
+    <Link to="/account">Account</Link>
+
+
   </Layout>
 )
 
